a594fbd83630473e03b5d2fab4e5b5741ff552cb
Richard <Richard@Gubijev.com>
2024-09-16 13:09:22 +0200

    add main_menu username input

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,8 +1,15 @@
 extends Control
 
 @onready var ip_line: LineEdit = $VBoxContainer/HBoxContainer/ip_line
+@onready var game: Node = $".."
+
+@onready var username_line: LineEdit = $VBoxContainer2/HBoxContainer2/username_line
+@onready var username_label: Label = $VBoxContainer2/HBoxContainer/username_label
 
 var PORT = 25565
+
+func _ready() -> void:
+	username_label.text = game.username
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
@@ -18,3 +25,6 @@ func _on_join_button_pressed() -> void:
 	peer.create_client(ip_line.text, PORT)
 	multiplayer.multiplayer_peer = peer
 
+func _on_set_username_button_pressed() -> void:
+	game.username = username_line.text
+	username_label.text = game.username
